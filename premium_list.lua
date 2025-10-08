639cfaf54d3c289aeaea9e2b4db687fbef1187eb
Freeman4i37 <trulyn300@gmail.com>
2025-10-08 20:51:29 -0300

    Criar premium_list.lua

---
+++ premium_list.lua
@@ -0,0 +1,39 @@
+return {
+    
+    Premium5 = {
+        
+    },
+
+    -
+    Premium4 = {
+    
+    },
+
+    
+    Premium3 = {
+
+    },
+
+
+    Premium2 = {
+        
+    },
+
+    
+    Premium1 = {
+    },
+
+    
+    OWNER = {
+        ["Kaua_452"] = true,
+    },
+    OWNER2 = {
+        ["pedro312jee"] = true,
+    },
+    MODDER = {
+        
+    },
+    STAFF = {
+        
+    }
+}
