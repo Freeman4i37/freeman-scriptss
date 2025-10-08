2715b15013da7408d0884e321e40607ec960c40e
Freeman4i37 <trulyn300@gmail.com>
2025-10-08 20:56:15 -0300

    Atualizar o premium_list.lua

--- premium_list.lua
+++ premium_list.lua
@@ -1,39 +1,15 @@
 return {
-    
-    Premium5 = {
-        
-    },
-
-    -
-    Premium4 = {
-    
-    },
-
-    
-    Premium3 = {
-
-    },
-
-
-    Premium2 = {
-        
-    },
-
-    
-    Premium1 = {
-    },
-
-    
+    Premium5 = {},
+    Premium4 = {},
+    Premium3 = {},
+    Premium2 = {},
+    Premium1 = {},
     OWNER = {
         ["Kaua_452"] = true,
     },
     OWNER2 = {
         ["pedro312jee"] = true,
     },
-    MODDER = {
-        
-    },
-    STAFF = {
-        
-    }
+    MODDER = {},
+    STAFF = {}
 }
