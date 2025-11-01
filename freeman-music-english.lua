ba8d8ad879e21142df1d0e0c63c9d01a107c2e00
Freeman4i37 <trulyn300@gmail.com>
2025-11-01 14:56:02 -0300

    Atualizar o freeman-music-english.lua

--- freeman-music-english.lua
+++ freeman-music-english.lua
@@ -1471,8 +1471,6 @@ do
 
         {name="Execute Auralynx Premium", url="https://raw.githubusercontent.com/TheAnnonimated/freeman/main/veri.lua"},
 
-        {name="Execute Brutus Hub", url="https://raw.githubusercontent.com/codenixstudios/brutus-hub/BRUTUS_TEAM/Games/brookhaven.lua"},
-
         {name="Execute Coquette Hub", url="https://ghostbin.axel.org/paste/vkhh2/raw"},
 
         {name="Execute AFEM", url="https://rawscripts.net/raw/Universal-Script-AFEM-Max-Open-Alpha-50210"},
