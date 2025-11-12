c61af2120fec340c8caca93811d7aed48886169a
Freeman <trulyn300@gmail.com>
2025-11-12 19:41:49 -0300

    Criar Auralynxs.lua

---
+++ Auralynxs.lua
@@ -0,0 +1,16 @@
+    ----------///// SISTEMA-ADMIN /////------------ 
+allowedUsers = {
+    ----------///// TIK-TOK /////------------           
+    
+    
+    
+    ----------///// TEMPORARIOS /////-------------
+    ----------////["test"]     = true, -- 03/10 03/12/////--------
+
+   ----------///// PERMANENTE ACESS /////------------
+    ["Kaua_452"]           = true,
+    ["kayke_nvconta2"]         = true,
+            
+}
+
+return allowedUsers
