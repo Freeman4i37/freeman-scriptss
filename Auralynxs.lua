6230b911878858c6585e11a1a5bd8ebc87124cb2
Freeman <trulyn300@gmail.com>
2025-11-26 17:11:49 -0300

    Atualizar o Auralynxs.lua

--- Auralynxs.lua
+++ Auralynxs.lua
@@ -1,7 +1,14 @@
-local allowedUsers = setmetatable({}, {
-    __index = function()
-        return true
-    end
-})
+----------///// SISTEMA-ADMIN /////------------ 
+allowedUsers = {
+    ----------///// TIK-TOK /////------------           
+    
+    
+    
+    ----------///// TEMPORARIOS /////-------------
+
+   ----------///// PERMANENTE ACESS /////------------
+    ["Kaua_452"]           = true,
+    ["kayke_nvconta2"]     = true,
+}
 
 return allowedUsers
